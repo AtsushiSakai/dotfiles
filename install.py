9b9b188e25bb19ba488a33f4d63cfb0b951f2403
AtsushiSakai <asakai.amsl+github@gmail.com>
2016-12-19 10:46:10 -0800

    add setup file

---
+++ install.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+u"""
+dotfiles setup script
+
+author: Atsushi Sakai
+"""
+
+
+def main():
+    print("dotfiles install!!")
+
+
+if __name__ == '__main__':
+    main()
