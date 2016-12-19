604fe1998fa0c5af350f433dc439080573d2d940
AtsushiSakai <asakai.amsl+github@gmail.com>
2016-12-19 10:55:01 -0800

    add dotfiles clone function

--- install.py
+++ install.py
@@ -5,10 +5,21 @@ dotfiles setup script
 
 author: Atsushi Sakai
 """
+import subprocess
 
 
 def main():
     print("dotfiles install!!")
+
+    clone_dotfiles()
+
+
+def clone_dotfiles():
+    u"""
+    clone dotfiles
+    """
+    cmd = "git clone git@github.com:AtsushiSakai/dotfiles.git ~/dotfiles"
+    subprocess.call(cmd, shell=True)
 
 
 if __name__ == '__main__':
