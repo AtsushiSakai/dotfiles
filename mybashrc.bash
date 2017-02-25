c846c9af8e704dfdda62295311d671451da44d12
AtsushiSakai <asakai.amsl+github@gmail.com>
2017-02-25 09:30:36 -0800

    add bash history share setting

--- mybashrc.bash
+++ mybashrc.bash
@@ -3,6 +3,8 @@
 # author: Atsushi Sakai
 # echo "Source mybashrc"
 
+export HISTSIZE=10000
+
 # for pyenv
 # export PATH="$HOME/.pyenv/bin:$PATH"
 eval "$(pyenv init -)"
@@ -12,3 +14,12 @@ source ~/dotfiles/mypercol.bash
 
 # for enhancd setting
 source ~/dotfiles/src/enhancd/init.sh
+
+# share bash history
+function share_history {
+    history -a
+    history -c
+    history -r
+}
+PROMPT_COMMAND='share_history'
+shopt -u histappend
