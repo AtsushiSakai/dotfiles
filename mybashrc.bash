b09678f0b6520de008065a2af6187fb3c46109c9
Atsushi Sakai <asakai.amsl+github@gmail.com>
2017-08-07 23:23:34 -0700

    git file check is added

--- mybashrc.bash
+++ mybashrc.bash
@@ -31,6 +31,8 @@ if [ -x "`which pyenv `" ]; then
     eval "$(pyenv init -)"
 fi 
 
-source /usr/local/etc/bash_completion.d/git-prompt.sh
-source /usr/local/etc/bash_completion.d/git-completion.bash
+if [ -e /usr/local/etc/bash_completion.d/git-prompt.sh ]; then
+    source /usr/local/etc/bash_completion.d/git-prompt.sh
+    source /usr/local/etc/bash_completion.d/git-completion.bash
+fi
 
