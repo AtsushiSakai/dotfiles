e61677ca6fe5887743bb047a01ec4c4f70c8b76f
AtsushiSakai <you@example.com>
2019-10-07 15:27:58 +0900

    add new update_all

---
+++ update_all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "$(basename $0) start!"
+
+echo "Update dotfiles"
+git pull origin master
+git pull --recurse-submodules origin master
+git submodule update --init --recursive
+git submodule foreach git pull origin master
+myvim/update_all.sh
+
+# Update all python libraries
+pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 sudo -H pip install -U --user
+
+echo "$(basename $0) done!"
+exit 0
+
