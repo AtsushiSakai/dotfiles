80e0b54a529041981a4e9c2b59d0e6a6e6bfd2ec
Atsushi Sakai <asakai.amsl+github@gmail.com>
2019-10-27 20:00:39 +0900

    update dotfiles

--- docker/jenkins.sh
+++ docker/jenkins.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "$(basename $0) start!"
-sudo docker run -d --name jenkins -p 8080:8080 -p 50000:50000 --restart=always -v ~/OneDrive/jenkins:/var/jenkins_home -v /var/run/docker.sock/:/bar/run/docker.sock -v ~/Dropbox/Program:/var/Program jenkins-master
+sudo docker run -d --name jenkins -p 8080:8080 -p 50000:50000 --restart=always -v ~/OneDrive/jenkins:/var/jenkins_home -v /var/run/docker.sock/:/var/run/docker.sock -v ~/Dropbox/Program:/var/Program jenkins-master
 echo "$(basename $0) done!"
 exit 0
 
--- vim/.vim/snippets/julia.jl
+++ vim/.vim/snippets/julia.jl
@@ -4,16 +4,13 @@
  author: Atsushi Sakai
 "
 
-using Test
-const __MAIN__ = length(PROGRAM_FILE)!=0 && occursin(PROGRAM_FILE, @__FILE__)
-
 function main()
     println(PROGRAM_FILE," start!!")
 
     println(PROGRAM_FILE," Done!!")
 end
 
-if __MAIN__
+if abspath(PROGRAM_FILE) == @__FILE__
     @time main()
 end
 
