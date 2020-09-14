5aa5458bfeb45f9081bc7a1ef03aa3b0299f44c0
Atsushi Sakai <KM60728@dir.kmt.kmtg.net>
2020-09-14 11:04:29 +0900

    add windows specific setting

---
+++ mybashrc_win.bash
@@ -0,0 +1,8 @@
+# Git bash setting on windows10
+export LANG=ja_JP.UTF-8
+alias percol='fzf'
+alias ls='ls --color=auto'
+
+# win sudo setting
+[ -f "${HOME}/bin/win-sudo/s/path.sh" ] && source "${HOME}/bin/win-sudo/s/path.sh"
+
