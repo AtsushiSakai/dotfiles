9751a9acedbad7d3b69acd84eb633c76b43bec99
Atsushi Sakai <KM60728@dir.kmt.kmtg.net>
2020-09-14 11:14:39 +0900

    add Open alias

--- mybashrc_win.bash
+++ mybashrc_win.bash
@@ -2,6 +2,7 @@
 export LANG=ja_JP.UTF-8
 alias percol='fzf'
 alias ls='ls --color=auto'
+alias Open='explorer.exe'
 
 # win sudo setting
 [ -f "${HOME}/bin/win-sudo/s/path.sh" ] && source "${HOME}/bin/win-sudo/s/path.sh"
